0aa90e4aa6341f7dc68ee52f28747b4fab9cb6b9
Madhav Madhusoodanan <f20200049@pilani.bits-pilani.ac.in>
2022-06-07 02:35:42 +0530

    roughly sorted out navbar ka css

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import Footer from "../components/footer"
 import Hero from "../components/hero/index"
+import Navbar from "../components/navbar"
 
 export default function Home() {
     return (
         <div className="bg-bg h-screen w-screen">
+            <Navbar />
             <Hero />
             <Footer />
         </div>
