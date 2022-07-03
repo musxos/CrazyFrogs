41d02db92e40ddd9a2405a879ee0d3c436918f52
codeturk <34295148+musxos@users.noreply.github.com>
2022-07-03 12:06:13 +0300

    Update index.jsx

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -4,5 +4,15 @@ const Footer = (props) => (
         <img src="/badge.svg" />
         <p className="text-white">NO RIGHTS RESERVED</p>
     </div>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-231713937-1">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-231713937-1');
+</script>
 )
 export default Footer
