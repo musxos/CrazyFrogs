1defdef560d94e0bd643dffef2b5b1bb1d37ce07
ChainMasterTR - Mustafa SARI <34295148+musxos@users.noreply.github.com>
2024-03-16 03:07:14 +0300

    Update index.jsx

--- components/hero/index.jsx
+++ components/hero/index.jsx
@@ -35,13 +35,13 @@ const Hero = () => (
                     </p>
                     <div className="w-full flex justify-start gap-4 lg:gap-4">
                        
-                        <a href="https://opensea.io/collection/crazyfrogsnfts" target="_blank">
+                        <a href="https://opensea.io/collection/crazyfrogsnfts" target="_blank" rel="noreferrer" >
                         <div className="rounded-full py-3 w-max text-center text-white text-md px-6 flex gap-4 lg:py-2 border-[1px] border-white">
                             <img src="/opensea.svg" className="text-black" />
                             <p className="w-max my-auto">Opensea</p>
                         </div>
                         </a>
-                        <a href="https://tofunft.com/collection/crazy-frogs-polygon/items" target="_blank">
+                        <a href="https://tofunft.com/collection/crazy-frogs-polygon/items" target="_blank" rel="noreferrer" >
                         <div className="rounded-full py-3 w-max text-center text-white text-md px-6 flex gap-4 lg:py-2 border-[1px] border-white">
                             <img src="/tofu.svg" className="text-black" />
                             <p className="w-max my-auto">tofu.NFT</p>
