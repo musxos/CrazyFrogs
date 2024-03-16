7bfc67da3937da5bd8eaf5565815e89773528495
ChainMasterTR - Mustafa SARI <34295148+musxos@users.noreply.github.com>
2024-03-16 23:14:01 +0300

    Update index.jsx

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -2,7 +2,7 @@
 const Footer = (props) => (
     <div {...props}>
         <img src="/badge.svg" />
-        <p className="text-white">NO RIGHTS RESERVED</p>
+        <p className="text-white">NO RIGHTS RESERVED - $CRZY IS A MEMECOIN AND HAS NO UTILITY. DON'T RISK MONEY YOU ARE AFRAID OF LOSING. THE PRICE MAY GO UP OR IT MAY GO DOWN. WE ARE NOT RESPONSIBLE FOR THE PRICE OF THE TOKEN.</p>
     </div>
 )
 export default Footer
