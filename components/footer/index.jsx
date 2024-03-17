031aef846e2bb1c5fd69eca9ec1a0b124b637718
ChainMasterTR - Mustafa SARI <34295148+musxos@users.noreply.github.com>
2024-03-17 05:15:34 +0300

    Update index.jsx

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -2,15 +2,11 @@
 const Footer = (props) => (
     <div {...props}>
 
-      <div className="text-white text-bold text-center text-sm">
-    <p>DISCLAIMER -</p>
-</div>
+   
 <div className="text-white text-xs">
-    <p>$CRZY IS A MEMECOIN AND HAS NO UTILITY. DONT RISK MONEY YOU ARE AFRAID OF LOSING.</p>
+    <p>$CRZY IS A MEMECOIN AND HAS NO UTILITY. DONT RISK MONEY YOU ARE AFRAID OF LOSING. THE PRICE MAY GO UP OR IT MAY GO DOWN. WE ARE NOT RESPONSIBLE FOR THE PRICE OF THE TOKEN.</p>
 </div>
-<div className="text-white text-xs ">
-    <p>THE PRICE MAY GO UP OR IT MAY GO DOWN. WE ARE NOT RESPONSIBLE FOR THE PRICE OF THE TOKEN.</p>
-</div>
+
 
     </div>
 )
