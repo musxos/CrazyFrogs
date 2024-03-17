82761834a0dce27bf0617f88e1fee1994ce91d77
ChainMasterTR - Mustafa SARI <34295148+musxos@users.noreply.github.com>
2024-03-17 04:25:32 +0300

    Update index.jsx

--- components/footer/index.jsx
+++ components/footer/index.jsx
@@ -2,15 +2,16 @@
 const Footer = (props) => (
     <div {...props}>
 
-        <div class="text-white text-bold text-center text-sm">
-            <p>DISCLAIMER</p>
-        </div>
-        <div class="text-white text-xs">
-            <p>$CRZY IS A MEMECOIN AND HAS NO UTILITY. DON'T RISK MONEY YOU ARE AFRAID OF LOSING. </p>
-        </div>
-        <div class="text-white text-xs">
-            <p>THE PRICE MAY GO UP OR IT MAY GO DOWN. WE ARE NOT RESPONSIBLE FOR THE PRICE OF THE TOKEN.</p>
-        </div>
+      <div className="text-white text-bold text-center text-sm">
+    <p>DISCLAIMER</p>
+</div>
+<div className="text-white text-xs">
+    <p>$CRZY IS A MEMECOIN AND HAS NO UTILITY. DON'T RISK MONEY YOU ARE AFRAID OF LOSING.</p>
+</div>
+<div className="text-white text-xs">
+    <p>THE PRICE MAY GO UP OR IT MAY GO DOWN. WE ARE NOT RESPONSIBLE FOR THE PRICE OF THE TOKEN.</p>
+</div>
+
     </div>
 )
 export default Footer
